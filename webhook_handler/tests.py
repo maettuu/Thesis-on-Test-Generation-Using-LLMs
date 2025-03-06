cdee58b8815a899702c265ea0b03c2f4fe833d92
maettuu <mangori41@bluewin.ch>
2025-03-06 15:52:25 +0100

    complete test setup

--- webhook_handler/tests.py
+++ webhook_handler/tests.py
@@ -228,3 +228,38 @@ class TestGenerationBugbot2588_cachedTest(TestCase):
         self.assertIsNotNone(response)  # Ensure response is not None
         self.assertTrue(isinstance(response, dict) or hasattr(response, 'status_code'))  # Ensure response is a dict or HttpResponse
 
+
+class TestGenerationPdfJs19232(TestCase):
+    def setUp(self):
+        # Load the local JSON file
+        mock_payload = "test_mocks/pdf_js_19232.json"
+        payload_path = os.path.join(os.path.dirname(__file__), mock_payload)
+        with open(payload_path, "r") as f:
+            self.payload = json.load(f)
+
+    def test_run_function_with_local_payload(self):
+        iAttempt = 0
+        stop = False  # we stop when successful
+        timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        while iAttempt < len(PROMPT_COMBINATIONS_GEN["include_golden_code"]) and not stop:
+            response, stop = run(self.payload,
+                                 model="gpt-4o",
+                                 iAttempt=iAttempt,
+                                 timestamp=timestamp,
+                                 post_comment=False)
+
+            iAttempt += 1
+
+        iAttempt = 0
+        while iAttempt < len(PROMPT_COMBINATIONS_GEN["include_golden_code"]) and not stop:
+            response, stop = run(self.payload,
+                                 model="meta-llama/Llama-3.3-70B-Instruct",
+                                 iAttempt=iAttempt,
+                                 timestamp=timestamp,
+                                 post_comment=False)
+
+            iAttempt += 1
+
+        self.assertIsNotNone(response)  # Ensure response is not None
+        self.assertTrue(
+            isinstance(response, dict) or hasattr(response, 'status_code'))  # Ensure response is a dict or HttpResponse
