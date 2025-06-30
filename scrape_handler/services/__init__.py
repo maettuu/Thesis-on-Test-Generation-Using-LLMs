2af8db25f8fc8f74727b23b5338d2ac4baaf7b10
maettuu <mangori41@bluewin.ch>
2025-06-30 18:05:58 +0200

    remove dead code

--- scrape_handler/services/__init__.py
+++ scrape_handler/services/__init__.py
@@ -1,6 +1,5 @@
 from .cst_builder     import CSTBuilder
 from .docker_service  import DockerService
-from .file_slicer     import GoldenFileSlicer
 from .gh_api          import GitHubApi
 from .llm_handler     import LLMHandler
 from .pr_diff_context import PullRequestDiffContext
@@ -9,7 +8,6 @@ from .test_generator  import TestGenerator
 __all__ = [
     "CSTBuilder",
     "DockerService",
-    "GoldenFileSlicer",
     "GitHubApi",
     "LLMHandler",
     "PullRequestDiffContext",
